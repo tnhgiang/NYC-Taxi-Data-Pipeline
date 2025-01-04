9bb5da6f823055284089717288377a6ed538ff71
tnhgiang <giang.tnhoang@gmail.com>
2025-01-04 21:54:26 +0700

    feat: Finish data cleaning Data cleaning for taxi zone and taxi zone geometry

--- pipeline/pipeline/assets/silver_layer.py
+++ pipeline/pipeline/assets/silver_layer.py
@@ -1,7 +1,10 @@
+from itertools import chain
+
+import geopandas as gpd
 import polars as pl
 from dagster import AssetExecutionContext, AssetIn, Output, asset
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import when
+from pyspark.sql.functions import col, create_map, lit, when
 
 from .. import constants as const
 from ..partitions import daily_partitions
@@ -14,11 +17,11 @@ from ..partitions import daily_partitions
         )
     },
     name="silver_cleaned_yellow_taxi_trips",
+    key_prefix=["silver", "nyc_taxi"],
+    group_name="silver",
     io_manager_key="spark_io_manager",
     required_resource_keys={"pyspark"},
-    key_prefix=["silver", "nyc_taxi"],
     compute_kind="Spark",
-    group_name="silver",
     partitions_def=daily_partitions,
 )
 def silver_cleaned_yellow_taxi_trips(
@@ -41,11 +44,15 @@ def silver_cleaned_yellow_taxi_trips(
     df = spark.createDataFrame(bronze_yellow_taxi_trips.to_dicts())
     df.cache()
 
-    context.log.info(f"Number of records before data cleaning: {df.count()}")
+    context.log.info(f"Number of rows before data cleaning: {df.count()}")
 
     # Deduplication
     df = df.dropDuplicates()
-    context.log.debug(f"Number of records after deduplication: {df.count()}")
+    context.log.debug(f"Number of rows after deduplication: {df.count()}")
+
+    # Drop rows with null value in any column
+    df.na.drop()
+    context.log.debug(f"Number of rows after dropping null: {df.count()}")
 
     # Drop redundant columns
     df = df.drop("store_and_fwd_flag")
@@ -80,6 +87,11 @@ def silver_cleaned_yellow_taxi_trips(
     # Drop rows where data points break range constraints
     df = (
         df.filter(
+            (df["VendorID"] >= const.VENDER_ID_MIN_VALUE)
+            & (df["VendorID"] <= const.VENDER_ID_MAX_VALUE)
+        )
+        .filter(df["passenger_count"] >= 0)
+        .filter(
             (df["RatecodeID"] >= const.RATECODEID_MIN_VALUE)
             & (df["RatecodeID"] <= const.RATECODEID_MAX_VALUE)
         )
@@ -94,7 +106,7 @@ def silver_cleaned_yellow_taxi_trips(
         .filter(df["congestion_surcharge"] >= 0)
         .filter(df["Airport_fee"] >= 0)
     )
-    context.log.debug(f"Number of records after data range constraints: {df.count()}")
+    context.log.debug(f"Number of rows after data range constraints: {df.count()}")
 
     # Set data point that breaks range constraints to default value
     df = df.withColumn(
@@ -106,9 +118,28 @@ def silver_cleaned_yellow_taxi_trips(
         ).otherwise(df["payment_type"]),
     )
 
+    # Change id to actual values
+    vendor_mapping = create_map(
+        [lit(value) for value in chain(*const.VENDOR_NAMES.items())]
+    )
+    ratecode_mapping = create_map(
+        [lit(value) for value in chain(*const.RATECODES.items())]
+    )
+    payment_type_mapping = create_map(
+        [lit(value) for value in chain(*const.PAYMENT_TYPES.items())]
+    )
+    df = df.withColumn(
+        "vendor",
+        vendor_mapping[col("VendorID")],
+    )
+    df = df.withColumn("ratecode", ratecode_mapping[col("RatecodeID")])
+    df = df.withColumn("payment_type", payment_type_mapping[col("payment_type")])
+
+    # Drop redundant columns
+    df = df.drop("VendorID", "RatecodeID")
+
     # Rename columns
     column_names_need_to_be_renamed_list = [
-        "VendorID",
         "tpep_pickup_datetime",
         "tpep_dropoff_datetime",
         "PULocationID",
@@ -116,7 +147,6 @@ def silver_cleaned_yellow_taxi_trips(
         "Airport_fee",
     ]
     new_column_names_list = [
-        "vendor_id",
         "pickup_datetime",
         "dropoff_datetime",
         "pickup_location_id",
@@ -134,7 +164,120 @@ def silver_cleaned_yellow_taxi_trips(
     ) / 60
     df = df.withColumn("trip_duration", trip_duration_in_minutes)
 
-    context.log.info(f"Number of records after data cleaning: {df.count()}")
+    # Sort by trip_id
+    df = df.orderBy("trip_id")
+
+    context.log.info(f"Number of rows after data cleaning: {df.count()}")
     df.unpersist()
 
     return Output(df, metadata={"row_count": df.count(), "column_names": df.columns})
+
+
+@asset(
+    ins={
+        "bronze_taxi_zone": AssetIn(
+            key_prefix=["bronze", "nyc_taxi"],
+        )
+    },
+    name="silver_cleaned_taxi_zone",
+    key_prefix=["silver", "nyc_taxi"],
+    group_name="silver",
+    io_manager_key="parquet_io_manager",
+    compute_kind="polars",
+)
+def silver_cleaned_taxi_zone(
+    context: AssetExecutionContext, bronze_taxi_zone: pl.DataFrame
+) -> Output[pl.DataFrame]:
+    df = bronze_taxi_zone
+    context.log.info(f"Number of rows before data cleaning: {df.height}")
+
+    # Deduplication
+    df = df.unique()
+    context.log.debug(f"Number of rows after deduplication: {df.height}")
+
+    # Drop rows with null value in any column
+    df = df.drop_nulls()
+    context.log.debug(f"Number of rows after dropping null: {df.height}")
+
+    # Drop redundant columns
+    df = df.drop(["service_zone"])
+
+    # Data type corrections
+    df = df.cast(
+        {
+            "LocationID": pl.UInt32,
+            "Borough": pl.String,
+            "Zone": pl.String,
+        }
+    )
+
+    # Rename columns
+    df = df.rename(
+        {
+            "LocationID": "location_id",
+            "Borough": "borough",
+            "Zone": "zone",
+        }
+    )
+
+    # Sort by location_id
+    df = df.sort("location_id")
+
+    context.log.info(f"Number of rows after data cleaning: {df.height}")
+
+    return Output(df, metadata={"row_count": df.height, "column_names": df.columns})
+
+
+@asset(
+    ins={
+        "bronze_taxi_zone_geometry": AssetIn(
+            key_prefix=["bronze", "nyc_taxi"],
+        )
+    },
+    name="silver_cleaned_taxi_zone_geometry",
+    key_prefix=["silver", "nyc_taxi"],
+    group_name="silver",
+    io_manager_key="parquet_io_manager",
+    compute_kind="Polars",
+)
+def silver_cleaned_taxi_zone_geometry(
+    context: AssetExecutionContext, bronze_taxi_zone_geometry: gpd.GeoDataFrame
+) -> Output[pl.DataFrame]:
+    # Compute longitude and latitude
+    df = bronze_taxi_zone_geometry.to_crs("EPSG:4326")
+    df["longitude"] = df.geometry.centroid.x
+    df["latitude"] = df.geometry.centroid.y
+
+    # Convert GeoDataFrame to Polars DataFrame
+    df = pl.from_pandas(df.drop(columns=["geometry", "Shape_Leng", "Shape_Area"]))
+
+    # Deduplication
+    df = df.unique()
+    context.log.debug(f"Number of rows after deduplication: {df.height}")
+
+    # Drop rows with null value in any column
+    df = df.drop_nulls()
+    context.log.debug(f"Number of rows after dropping null: {df.height}")
+
+    # Data type corrections
+    df = df.cast(
+        {
+            "OBJECTID": pl.UInt32,
+            "longitude": pl.Float32,
+            "latitude": pl.Float32,
+        }
+    )
+
+    # Rename columns
+    df = df.rename(
+        {
+            "OBJECTID": "location_id",
+        }
+    )
+
+    # Sort by location_id
+    df = df.sort("location_id")
+
+    context.log.debug(f"Number of rows after data cleaning: {df.height}")
+
+    return Output(df, metadata={"row_count": df.height, "column_names": df.columns})
