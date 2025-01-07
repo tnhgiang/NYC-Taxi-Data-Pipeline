18ab5eb6a014baee03cb0cf81d7e47b9b38e260d
tnhgiang <giang.tnhoang@gmail.com>
2025-01-07 15:01:19 +0700

    build: Fix network, port and table init

--- clickhouse/docker-entrypoint-initdb.d/00-create-dim-locations-table.sh
+++ clickhouse/docker-entrypoint-initdb.d/00-create-dim-locations-table.sh
@@ -7,8 +7,12 @@ clickhouse client -n <<EOSQL
 CREATE TABLE IF NOT EXISTS ${CLICKHOUSE_DB}.dim_locations
 (
     location_id UInt32,
-    location_name String,
-    location_type String
+    borough String,
+    zone String,
+    shape_length Float32,
+    shape_area Float32,
+    longitude Float32,
+    latitude Float32
 )
 ENGINE = MergeTree()
 PRIMARY KEY location_id;
